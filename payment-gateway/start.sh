4d8e6f9135ef1ba20a5a91715e711ffc053082c5
Mijael Balderrama <micho.balducas@outlook.com>
2024-04-19 17:31:26 -0400

    added some lines

--- payment-gateway/start.sh
+++ payment-gateway/start.sh
@@ -8,13 +8,17 @@ if [ ! -f "db/migration/db-up.sql" ]; then
     exit 1
 fi
 
-echo "$DB_HOST"
-echo "$DB_PORT"
-echo "$DB_USER"
-echo "$DB_NAME"
+echo "Host: $DB_HOST"
+echo "port: $DB_PORT"
+echo "user: $DB_USER"
+echo "db Name: $DB_NAME"
+echo "password: $DB_PASSWORD"
+
+# export PWD=$DB_PASSWORD
+# psql -U "$DB_USER" -d payment_service -f db/migration/db-up.sql
 
 # Connect to the PostgreSQL database
-psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -w -c "SELECT 1" > /dev/null 2>&1
+psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -w -c --password="$DB_PASSWORD" "SELECT 1" > /dev/null 2>&1
 
 if [ $? -ne 0 ]; then
     echo "Error: Could not connect to PostgreSQL database."
@@ -22,7 +26,7 @@ if [ $? -ne 0 ]; then
 fi
 
 # Execute the db-up.sql file
-psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" < "db/migration/db-up.sql"
+psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -W password="$DB_PASSWORD"< "db/migration/db-up.sql"
 
 if [ $? -ne 0 ]; then
     echo "Error: Could not execute db-up.sql file."
