e331bab857653826cec5172339a748e68afaa545
Mijael Balderrama <micho.balducas@outlook.com>
2024-04-22 09:54:15 -0400

    fixed all problems

--- payment-gateway/README.md
+++ payment-gateway/README.md
@@ -1,1 +1,17 @@
-docker build -t payment .
+# Payment service 
+## Overview
+This respository was created by Mijael Balderrama for a payment service, this service send a comunication to another bank service 
+
+## how to do it.
+
+1. `install docker`
+2. `go to payment-gateway folder`
+3. `execute the comand:`
+
+        docker compose up
+    this command will compile the application and rise up the database
+4. in the project folder go to `db/migration/db-up.sql` file and copy the content
+5. open any postgressql manager `I recomend dbeaver-ce`
+
+5. connet to database, all credentials are in `env` file
+6. open a sql-script and paste the sql script and execute
--- payment-gateway/start.sh
+++ payment-gateway/start.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 
 source ./app.env
-echo "$DB_SOURCE"
 # Check if db-up.sql file exists
 if [ ! -f "db/migration/db-up.sql" ]; then
     echo "Error: db-up.sql file not found."
     exit 1
 fi
 
-echo "Host: $DB_HOST"
-echo "port: $DB_PORT"
-echo "user: $DB_USER"
-echo "db Name: $DB_NAME"
-echo "password: $DB_PASSWORD"
-
-# export PWD=$DB_PASSWORD
-# psql -U "$DB_USER" -d payment_service -f db/migration/db-up.sql
+echo "starting migration..."
 
 # Connect to the PostgreSQL database
-psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -w -c --password="$DB_PASSWORD" "SELECT 1" > /dev/null 2>&1
+# PGPASSWORD="$DB_PASSWORD" psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -w -c "SELECT 1" > /dev/null 2>&1
+RUN_ON_MYDB="psql -X -U $DB_USER password=$DB_PASSWORD --set ON_ERROR_STOP=on"
 
 if [ $? -ne 0 ]; then
     echo "Error: Could not connect to PostgreSQL database."
@@ -26,8 +19,8 @@ if [ $? -ne 0 ]; then
 fi
 
 # Execute the db-up.sql file
-psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -W password="$DB_PASSWORD"< "db/migration/db-up.sql"
-
+# PGPASSWORD="$DB_PASSWORD" psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" < "db/migration/db-up.sql"
+RUN_ON_MYDB="psql -X -U $DB_USER password=$DB_PASSWORD --set ON_ERROR_STOP=on" < "db/migration/db-up.sql"
 if [ $? -ne 0 ]; then
     echo "Error: Could not execute db-up.sql file."
     exit 1
